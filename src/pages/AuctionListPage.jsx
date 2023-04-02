35794dde127aa462602b05474a844f205e62a325
e1kim <91405467+e1kim@users.noreply.github.com>
2023-04-02 15:00:30 +0900

    color 에러 수정

--- src/pages/AuctionListPage.jsx
+++ src/pages/AuctionListPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import { colors } from '../config/color';
+
 
 import { db, authService, storage } from '../config/firebase';
 import useUser from '../hooks/useUser';
