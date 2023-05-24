422941e80de77cfd24f9872dd9562fd9df726c48
Ahyoung-Kim <eileel@naver.com>
2023-05-24 15:24:39 +0900

    useIsLike 오류: 예외 처리 코드 추가

--- src/components/exchange/ExchangeContainer.jsx
+++ src/components/exchange/ExchangeContainer.jsx
@@ -10,22 +10,28 @@ import { ImageSizeTable } from "../common/ProductImg";
 import TagList from "../common/TagList";
 
 const ExchangeContainer = ({ data }) => {
-
   const navigate = useNavigate();
-  const goAuctionDetailPage = () => {
+  const goExchangeDetailPage = () => {
     navigate(`/exchange/exchangedetail/${data.id}`);
   };
   return (
     <ProductContainer>
-      <ProductImg product={data}  onClick={goAuctionDetailPage} auction={false}/>
+      <ProductImg
+        product={data}
+        onClick={goExchangeDetailPage}
+        auction={false}
+      />
 
       <ProductOwner uid={data.uid} />
 
-      <ProductTitle title={data.title}/>
+      <ProductTitle title={data.title} />
 
-      <ExchangeMember memberA={data.haveMember} memberB={data.wantMember}/>
+      <ExchangeMember memberA={data.haveMember} memberB={data.wantMember} />
 
-      <TagList width={ImageSizeTable["M"]} tags={[data.region, data.transactionType]} />
+      <TagList
+        width={ImageSizeTable["M"]}
+        tags={[data.region, data.transactionType]}
+      />
     </ProductContainer>
   );
 };
--- src/hooks/useHeartExchange.jsx
+++ src/hooks/useHeartExchange.jsx
@@ -1,155 +1,128 @@
-import React ,{ useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { doc, getDoc } from "firebase/firestore";
 import { db, increment, fieldvalue } from "../config/firebase";
 import Loading from "../components/common/Loading";
 
 // 데이터베이스에 like 저장
-export const useAddLike = async(userId, productId) => {
-   
-    const [arrayData, setArrayData] = useState([]);
-    const docRef = doc(db, "likes", userId);
+export const useAddLike = async (userId, productId) => {
+  const [arrayData, setArrayData] = useState([]);
+  const docRef = doc(db, "likes", userId);
 
+  const docSnap = await getDoc(docRef);
 
-    const docSnap = await getDoc(docRef);
-
-    if(docSnap.exists()){
-        const data = docSnap.data();
-        setArrayData(data.exchanges);
-        if(arrayData){
-            const index = arrayData.indexOf(productId);
-            if(index <0){
-                const newArrayData = [...arrayData, productId ];
-                const productDB = db.collection("likes");
-                await productDB.doc(userId).update({ products: newArrayData });
-            }
-            
-        }
+  if (docSnap.exists()) {
+    const data = docSnap.data();
+    setArrayData(data.exchanges);
+    if (arrayData) {
+      const index = arrayData.indexOf(productId);
+      if (index < 0) {
+        const newArrayData = [...arrayData, productId];
+        const productDB = db.collection("likes");
+        await productDB.doc(userId).update({ products: newArrayData });
+      }
     }
-    
-    
-    
+  }
 };
 
 // likes 컬렉션에 userId 도큐먼트가 없는 경우 새로운 도큐먼트 생성
 export const AddLikeDoc = async (userId) => {
-    const productDB = db.collection("likes");
-    await productDB.doc(userId).set({ products:[], exchanges: [] });
+  const productDB = db.collection("likes");
+  await productDB.doc(userId).set({ products: [], exchanges: [] });
 };
 
-
-
-//user의 like products 데이터 읽기 
+//user의 like products 데이터 읽기
 export const useLike = (user) => {
-    
-    const [arrayData, setArrayData] = useState([]);
-    
-    const getLike = async() =>{
-        try {
-            const docRef = doc(db, "likes", user.uid);
-            const docSnap = await getDoc(docRef);
-            if (!docSnap.exists()) {
-                await AddLikeDoc(user.uid);
-                return [];
-            }
-            else{
-                const data = docSnap.data();
-                setArrayData(data.exchanges);
-                
-            }
-          } catch (err) {
-            console.log("getLike error: ", err);
-          }
-    };
+  const [arrayData, setArrayData] = useState([]);
 
-    useEffect(() => {
-        getLike();
-    },[] );
-    
-    if(!user){
+  const getLike = async () => {
+    try {
+      const docRef = doc(db, "likes", user.uid);
+      const docSnap = await getDoc(docRef);
+      if (!docSnap.exists()) {
+        await AddLikeDoc(user.uid);
         return [];
+      } else {
+        const data = docSnap.data();
+        setArrayData(data.exchanges);
+      }
+    } catch (err) {
+      console.log("getLike error: ", err);
     }
-    
-    return arrayData;
+  };
+
+  useEffect(() => {
+    getLike();
+  }, []);
+
+  if (!user) {
+    return [];
+  }
+
+  return arrayData;
 };
 
+//user의 like products 데이터 읽기
+export const Like2 = async (user) => {
+  let arrayData = [];
 
-
-//user의 like products 데이터 읽기 
-export const Like2 = async(user) => {
-    
-    let arrayData = [];
-    
-    if (user){
-        const docRef = doc(db, "likes", user.uid);
-        const docSnap = await getDoc(docRef);
-        if (!docSnap.exists()) {
-            await AddLikeDoc(user.uid);
-            
-        }
-        else{
-            const data = docSnap.data();
-            arrayData = (data.exchanges);
-        }
+  if (user) {
+    const docRef = doc(db, "likes", user.uid);
+    const docSnap = await getDoc(docRef);
+    if (!docSnap.exists()) {
+      await AddLikeDoc(user.uid);
+    } else {
+      const data = docSnap.data();
+      arrayData = data.exchanges;
     }
-    
-    return arrayData;
+  }
+
+  return arrayData;
 };
-
-
 
 // removeLike 데이터 삭제
-export const useRemoveLike = async(userId, productId) => {
-    
-    const arrayData = useLike(userId);
+export const useRemoveLike = async (userId, productId) => {
+  const arrayData = useLike(userId);
+  const index = arrayData.indexOf(productId);
+  const productDB = db.collection("likes");
+
+  if (index >= 0) {
+    const newArrayData = [
+      ...arrayData.slice(0, index),
+      ...arrayData.slice(index + 1),
+    ];
+    await productDB.doc(userId).update({ exchanges: newArrayData });
+  }
+};
+
+export const useIsLike = (productId, arrayData) => {
+  if (!arrayData) return null;
+  if (arrayData.length) {
     const index = arrayData.indexOf(productId);
-    const productDB = db.collection("likes");
 
-    if (index >= 0) {
-        const newArrayData = [...arrayData.slice(0, index), 
-            ...arrayData.slice(index + 1)];
-        await productDB.doc(userId).update({ exchanges: newArrayData });
-    }
-    
-    
+    if (index >= 0) return true;
+    else return false;
+  } else {
+    return null;
+  }
 };
 
-export const useIsLike = (productId,arrayData) =>{
+export const useExchangeLike = async (productId) => {
+  const [likes, setLikes] = useState(0);
 
-    if (arrayData.length){
-        const index = arrayData.indexOf(productId);
-        
-        if (index >= 0) return true;
-        else return false;
-    }
-    else{
-        return null;
-    }
-    
-
+  const docRef = doc(db, "exchange", productId);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+    setLikes(docSnap.data().likes);
+  }
+  return likes;
 };
 
-export const useExchangeLike = async(productId) =>{
-
-    const [likes, setLikes] = useState(0);
-
-    const docRef = doc(db, "exchange", productId);
-    const docSnap = await getDoc(docRef);
-    if(docSnap.exists()){
-        setLikes(docSnap.data().likes);
-    }
-    return likes;
+export const plusProductLike = async (productId) => {
+  const docRef = db.collection("exchange").doc(productId);
+  if (docRef) await docRef.update({ likes: fieldvalue.increment(1) });
 };
 
-export const plusProductLike = async(productId) =>{
-    
-    const docRef = db.collection("exchange").doc(productId);
-    if (docRef) await docRef.update({ likes: fieldvalue.increment(1) });
-    
-}
-
-export const miusProductLike = async(productId) =>{
- 
-    const docRef = db.collection("exchange").doc(productId);
-    if (docRef) await docRef.update({ likes: fieldvalue.increment(-1) });
-    
-}
+export const miusProductLike = async (productId) => {
+  const docRef = db.collection("exchange").doc(productId);
+  if (docRef) await docRef.update({ likes: fieldvalue.increment(-1) });
+};
