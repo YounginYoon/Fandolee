6b32185420d5caa56ea20fcb7b2ae3c7e1213017
YounginYoon <duddlsdbs@sogang.ac.kr>
2023-06-02 19:09:23 +0900

    낙찰 채팅 내역 수정

--- src/components/mypage/chat/TransactionChattingList.jsx
+++ src/components/mypage/chat/TransactionChattingList.jsx
@@ -1,38 +1,55 @@
-import React, { useEffect, useState } from "react";
-import { db, realTimeDatabase } from "../../../config/firebase";
-import Loading from "../../common/Loading";
-import styled from "styled-components";
-import TransactionChattingBox from "./TransactionChattingBox";
-import EmptyList from "../EmptyList";
+import React, { useEffect, useState } from 'react';
+import { db, realTimeDatabase } from '../../../config/firebase';
+import { collection, query, where, getDocs, orderBy } from 'firebase/firestore';
+import Loading from '../../common/Loading';
+import styled from 'styled-components';
+import TransactionChattingBox from './TransactionChattingBox';
+import EmptyList from '../EmptyList';
 
 const TransactionChattingList = ({ user }) => {
   // 낙찰 후 거래 채팅의 상품 id 담음
   const [auctionTransaction, setAuctionTransaction] = useState(null);
   // 낙찰 거래 채팅 가져오기 (내가 올린 상품에서 포함)
   const getAuctionTransactionChats = async () => {
-    const chatRef = realTimeDatabase.ref("ChatRoom/Auction");
+    const chatRef = realTimeDatabase.ref('ChatRoom/Auction');
+    const productRef = query(collection(db, 'product'));
     try {
       const promises = [];
-      const products = await chatRef.once("value").then(async (snapshot) => {
+      const productsList = []; // 낙찰되거나 판매한 상품 담음
+      const productDocs = await getDocs(productRef);
+      await productDocs.forEach((doc) => {
+        const data = doc.data();
+        if (data.uid === user.uid || data.bidder === user.uid)
+          productsList.push(doc.id);
+      });
+      await chatRef.once('value').then(async (snapshot) => {
         snapshot.forEach((child) => {
-          child.forEach(async (snap) => {
-            const chat = snap.val();
-            if (chat.username === user.uid) {
-              promises.push(child.key);
-            }
+          productsList.forEach((productId) => {
+            if (child.key === productId) promises.push(child.key);
           });
         });
       });
+      // const products = await chatRef.once('value').then(async (snapshot) => {
+      //   snapshot.forEach((child) => {
+      //     child.forEach(async (snap) => {
+      //       const chat = snap.val();
+      //       if (chat.username === user.uid) {
+      //         promises.push(child.key);
+      //       }
+      //     });
+      //   });
+      // });
 
       //   console.log("transaction chats: ", promises);
       setAuctionTransaction(promises);
     } catch (err) {
-      console.log("getExchangeList err: ", err);
+      console.log('getExchangeList err: ', err);
     }
   };
 
   useEffect(() => {
     getAuctionTransactionChats();
+    //if (auctionTransaction) console.log(auctionTransaction);
   }, []);
 
   if (!auctionTransaction) {
