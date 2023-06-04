7c46fc0c4a2eafa103099ab4be6d97c74ffe48e7
YounginYoon <duddlsdbs@sogang.ac.kr>
2023-06-05 00:41:12 +0900

    RecommendedProducts.jsx 주석 처리

--- src/components/home/RecommendedProducts.jsx
+++ src/components/home/RecommendedProducts.jsx
@@ -8,116 +8,83 @@ import ExchangeList from '../exchange/ExchangeList';
 import { useNavigate } from 'react-router-dom';
 
 const RecommendedProducts = () => {
-  const productRef = query(collection(db, 'product'));
-  const exchangeRef = query(collection(db, 'exchange'));
-  // 모든 경매 상품 담음
-  const [products, setProducts] = useState([]);
-  // 모든 교환 상품 담음
-  const [exchange, setExchange] = useState([]);
-  //경매 랜덤 추천 상품 담음
-  const [recommendProducts, setRecommendProducts] = useState([]);
-  //교환 랜덤 추천 상품 담음
-  const [recommendExchange, setRecommendExchange] = useState([]);
-
-  const navigate = useNavigate();
-
-  // 모든 경매 상품 가져옴
-  const getAllProducts = async () => {
-    const productDocs = await getDocs(productRef);
-    const productsList = [];
-    await productDocs.forEach((doc) => {
-      const data = doc.data();
-      if (data.isComplete === 0) productsList.push({ id: doc.id, data });
-    });
-
-    setProducts(productsList);
-  };
-
-  //모든 교환 상품 가져옴
-  const getAllExchange = async () => {
-    const exchangeDocs = await getDocs(exchangeRef);
-    const productsList = [];
-    await exchangeDocs.forEach((doc) => {
-      const data = doc.data();
-      if (data.isComplete === 0) productsList.push({ id: doc.id, data });
-    });
-
-    setExchange(productsList);
-  };
-
-  const randomRecommend = (list, setRecommend) => {
-    const shuffled = list.sort(() => 0.5 - Math.random());
-    const selected = shuffled.slice(0, 3); // 랜덤으로 3개 뽑기
-    setRecommend(selected);
-  };
-
-  const goAuctionProduct = (productId) => {
-    navigate(`/auction/auctiondetail/${productId}`);
-  };
-
-  const goExchangeProduct = (productId) => {
-    navigate(`/exchange/exchangedetail/${productId}`);
-  };
-
-  useEffect(() => {
-    getAllProducts();
-    getAllExchange();
-  }, []);
-
-  useEffect(() => {
-    if (products.length > 0) {
-      randomRecommend(products, setRecommendProducts);
-    }
-    if (exchange.length > 0) {
-      randomRecommend(exchange, setRecommendExchange);
-      console.log(recommendExchange);
-    }
-  }, [products, exchange]);
-
+  // const productRef = query(collection(db, 'product'));
+  // const exchangeRef = query(collection(db, 'exchange'));
+  // // 모든 경매 상품 담음
+  // const [products, setProducts] = useState([]);
+  // // 모든 교환 상품 담음
+  // const [exchange, setExchange] = useState([]);
+  // //경매 랜덤 추천 상품 담음
+  // const [recommendProducts, setRecommendProducts] = useState([]);
+  // //교환 랜덤 추천 상품 담음
+  // const [recommendExchange, setRecommendExchange] = useState([]);
+  // const navigate = useNavigate();
+  // // 모든 경매 상품 가져옴
+  // const getAllProducts = async () => {
+  //   const productDocs = await getDocs(productRef);
+  //   const productsList = [];
+  //   await productDocs.forEach((doc) => {
+  //     const data = doc.data();
+  //     if (data.isComplete === 0) productsList.push({ id: doc.id, data });
+  //   });
+  //   setProducts(productsList);
+  // };
+  // //모든 교환 상품 가져옴
+  // const getAllExchange = async () => {
+  //   const exchangeDocs = await getDocs(exchangeRef);
+  //   const productsList = [];
+  //   await exchangeDocs.forEach((doc) => {
+  //     const data = doc.data();
+  //     if (data.isComplete === 0) productsList.push({ id: doc.id, data });
+  //   });
+  //   setExchange(productsList);
+  // };
+  // const randomRecommend = (list, setRecommend) => {
+  //   const shuffled = list.sort(() => 0.5 - Math.random());
+  //   const selected = shuffled.slice(0, 3); // 랜덤으로 3개 뽑기
+  //   setRecommend(selected);
+  // };
+  // const goAuctionProduct = (productId) => {
+  //   navigate(`/auction/auctiondetail/${productId}`);
+  // };
+  // const goExchangeProduct = (productId) => {
+  //   navigate(`/exchange/exchangedetail/${productId}`);
+  // };
+  // useEffect(() => {
+  //   getAllProducts();
+  //   getAllExchange();
+  // }, []);
+  // useEffect(() => {
+  //   if (products.length > 0) {
+  //     randomRecommend(products, setRecommendProducts);
+  //   }
+  //   if (exchange.length > 0) {
+  //     randomRecommend(exchange, setRecommendExchange);
+  //     console.log(recommendExchange);
+  //   }
+  // }, [products, exchange]);
   return (
     <Container>
       <Text>팬도리를 위한 추천상품 🎁</Text>
-
       <ProductsDiv>
-        {recommendProducts.map((product) => (
-          <ProductBox key={product.id}>
-            <ProductImg
-              src={product.data.images[0]}
-              onClick={() => goAuctionProduct(product.id)}
-            />
-          </ProductBox>
-        ))}
-        {recommendExchange.map((product) => (
-          <ProductBox key={product.id}>
-            <ProductImg
-              src={product.data.images[0]}
-              onClick={() => goExchangeProduct(product.id)}
-            />
-          </ProductBox>
-        ))}
-        {/* <ProductBox>
+        <ProductBox>
           <ProductImg src="/img/goods1.jpeg" />
         </ProductBox>
-
         <ProductBox>
           <ProductImg src="/img/goods2.jpeg" />
         </ProductBox>
-
         <ProductBox>
           <ProductImg src="/img/goods3.jpeg" />
         </ProductBox>
-
         <ProductBox>
           <ProductImg src="/img/goods4.jpeg" />
         </ProductBox>
-
         <ProductBox>
           <ProductImg src="/img/goods5.jpeg" />
         </ProductBox>
-
         <ProductBox>
           <ProductImg src="/img/goods6.jpeg" />
-        </ProductBox> */}
+        </ProductBox>
       </ProductsDiv>
     </Container>
   );
