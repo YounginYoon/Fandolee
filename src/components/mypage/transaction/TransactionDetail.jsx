a504996c8243dfbfc25dcf403f9fa5543cb0173d
Ahyoung-Kim <eileel@naver.com>
2023-06-07 16:32:55 +0900

    거래내역 디테일 페이지 정보 추가

--- src/components/mypage/transaction/TransactionDetail.jsx
+++ src/components/mypage/transaction/TransactionDetail.jsx
@@ -11,6 +11,8 @@ import moment from "moment";
 import Loading from "../../common/Loading";
 import nickName from "../../../hooks/nickName";
 import useUser from "../../../hooks/useUser";
+import { moneyFormat } from "../../../common/money";
+import { timestampToDateFormat } from "../../../common/date";
 
 const TransactionDetail = () => {
   const navigate = useNavigate();
@@ -25,21 +27,18 @@ const TransactionDetail = () => {
   const [consumer, setConsumer] = useState("");
 
   const setData = async (product) => {
-    if(product){
+    if (product) {
       const nickname = await nickName(product.uid);
       return nickname;
-    }
-    else{
+    } else {
       return null;
     }
-    
   };
   const setData2 = async (product) => {
-    if(product){
+    if (product) {
       const nickname = await nickName(product.bidder);
       return nickname;
-    }
-    else{
+    } else {
       return null;
     }
   };
@@ -47,29 +46,25 @@ const TransactionDetail = () => {
   const showNickName = () => {
     const seller2 = setData(product);
     const consumer2 = setData2(product);
-    if(seller2){
+    if (seller2) {
       seller2.then((id) => {
         setSeller(id);
         //console.log("seller: ",id);
       });
     }
-    if(consumer2){
-      consumer2.then((id)=>{
+    if (consumer2) {
+      consumer2.then((id) => {
         //console.log("consumer: ",id);
         setConsumer(id);
-      })
+      });
     }
-    
   };
-
-
 
   useEffect(() => {
     showNickName();
-    
   }, [product]);
 
-  if (!product) {
+  if (!product || !seller || !consumer) {
     return <Loading />;
   }
 
@@ -82,14 +77,20 @@ const TransactionDetail = () => {
 
       <InfoDiv>
         <TransactionInfoBox label="굿즈" image={product.images[0]} />
-        <TransactionInfoBox label="제목" text = {product.title} />
+        <TransactionInfoBox label="제목" text={product.title} />
         <TransactionInfoBox label="아이돌" text={product.idol} />
+        <TransactionInfoBox label="굿즈종류" text={product.category} />
+        <TransactionInfoBox label="거래유형" text={"경매"} />
         <TransactionInfoBox label="판매자" text={seller} />
-        <TransactionInfoBox label="낙찰 금액" text={product.biddingPrice} />
         <TransactionInfoBox label="낙찰자" text={consumer} />
         <TransactionInfoBox
+          label="낙찰 금액"
+          text={`${moneyFormat(product.biddingPrice)} 원`}
+        />
+
+        <TransactionInfoBox
           label="낙찰일"
-          text={moment(product.biddingDate.toDate()).format("L")}
+          text={timestampToDateFormat(product.biddingDate)}
         />
       </InfoDiv>
     </Container>
